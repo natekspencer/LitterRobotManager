3ec8bf75058bbb47b67f216156f52e82151ff625
Nathan Spencer <natekspencer@gmail.com>
2020-02-05 15:37:21 -0700

    updates to failure logic and reduce api calls when not logged in

--- devicetypes/natekspencer/litter-robot.src/litter-robot.groovy
+++ devicetypes/natekspencer/litter-robot.src/litter-robot.groovy
@@ -1,7 +1,7 @@
 /**
  *  Litter-Robot
  *
- *  Copyright 2019 Nathan Spencer
+ *  Copyright 2020 Nathan Spencer
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
